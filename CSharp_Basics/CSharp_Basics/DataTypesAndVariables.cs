a883bceef88b49245314b20fd3a9846c01b908fd
mwyrodek <wyrodek.maciej@gmail.com>
2019-09-25 12:29:50 +0200

    aaaa

--- CSharp_Basics/CSharp_Basics/DataTypesAndVariables.cs
+++ CSharp_Basics/CSharp_Basics/DataTypesAndVariables.cs
@@ -28,52 +28,57 @@ namespace CSharp_Basics
         //        /// singleDigit, number, lessThanZero
         //        /// make they values:  6   14  -100
         //        /// </summary>
-        //        [Test]
-        //        public void Creating_Integeres()
-        //        {
-        //            // int - describes type
-        //            // example - is name of variable, english names are preferred
-        //            // =  equal sign is used for assigning values.
-        //            // 0 - the value
-        //            //  ; - IMPORTANT ';' sign means and of the command usually it is on end of the line - remember about it
-        //            int example = 0;
-        //
-        //            Assert.AreEqual(example, 0, "Example Has wrong value did you changed it?");
-        //            Assert.AreEqual(singleDigit, 6, "Example Has wrong value did you changed it?");
-        //            Assert.That(singleDigit, Is.TypeOf<int>(), "You sure it is int?");
-        //            Assert.AreEqual(number, 14, "Example Has wrong value did you changed it?");
-        //            Assert.That(singleDigit, Is.TypeOf<int>(), "You sure it is int?");
-        //            Assert.AreEqual(lessThanZero, -100, "Example Has wrong value did you changed it?");
-        //            Assert.That(singleDigit, Is.TypeOf<int>(), "You sure it is int?");
-        //        }
+        [Test]
+        public void Creating_Integeres()
+        {
+            // int - describes type
+            // example - is name of variable, english names are preferred
+            // =  equal sign is used for assigning values.
+            // 0 - the value
+            //  ; - IMPORTANT ';' sign means and of the command usually it is on end of the line - remember about it
+            int example = 0;
+            int singleDigit = 6;
+            int number = 14;
+            int lessThanZero = -100;
 
-        //        /// <summary>
-        //        /// Basic operations at integers are
-        //        /// add(+), subtract(-), multiply(*), divide(/)
-        //        /// make integer variables: subtract, multiply and divide perform operations.
-        //        /// discuss why divide is not working
-        //        /// </summary>
-        //        [Test]
-        //        public void Operation_On_Integers()
-        //        {
-        //            int X = 4;
-        //            int Y = 5;
-        //
-        //            /*
-        //             * There are few other ways you can do this
-        //             * int add = 0;
-        //             * add = x+y;
-        //             */
-        //            int add = X + Y;
-        //
-        //            Assert.AreEqual(9, add, "Example Has wrong value did you changed it?");
-        //            Assert.AreEqual(-1, subtract, "Wrong value maybe other way around?");
-        //            Assert.That(subtract, Is.TypeOf<int>(), "You sure it is int?");
-        //            Assert.AreEqual(20, multiply, "Example Has wrong value did you changed it?");
-        //            Assert.That(multiply, Is.TypeOf<int>(), "You sure it is int?");
-        //            Assert.AreEqual(1.25, divide, "Example Has wrong value did you changed it?");
-        //            Assert.That(divide, Is.TypeOf<int>(), "You sure it is int?");
-        //        }
+            Assert.AreEqual(example, 0, "Example Has wrong value did you changed it?");
+            Assert.AreEqual(singleDigit, 6, "Example Has wrong value did you changed it?");
+            Assert.That(singleDigit, Is.TypeOf<int>(), "You sure it is int?");
+            Assert.AreEqual(number, 14, "Example Has wrong value did you changed it?");
+            Assert.That(singleDigit, Is.TypeOf<int>(), "You sure it is int?");
+            Assert.AreEqual(lessThanZero, -100, "Example Has wrong value did you changed it?");
+            Assert.That(singleDigit, Is.TypeOf<int>(), "You sure it is int?");
+        }
+
+        /// <summary>
+        /// Basic operations at integers are
+        /// add(+), subtract(-), multiply(*), divide(/)
+        /// make integer variables: subtract, multiply and divide perform operations.
+        /// discuss why divide is not working
+        /// </summary>
+        [Test]
+        public void Operation_On_Integers()
+        {
+            int X = 4;
+            int Y = 5;
+
+            /*
+             * There are few other ways you can do this
+             * int add = 0;
+             * add = x+y;
+             */
+            int add = X + Y;
+            int subtract = X - Y;
+            double divide =  (double)Y/X;
+            int multiply = X * Y;
+            Assert.AreEqual(9, add, "Example Has wrong value did you changed it?");
+            Assert.AreEqual(-1, subtract, "Wrong value maybe other way around?");
+            Assert.That(subtract, Is.TypeOf<int>(), "You sure it is int?");
+            Assert.AreEqual(20, multiply, "Example Has wrong value did you changed it?");
+            Assert.That(multiply, Is.TypeOf<int>(), "You sure it is int?");
+            Assert.AreEqual(1.25, divide, "Example Has wrong value did you changed it?");
+            Assert.That(divide, Is.TypeOf<double>(), "You sure it is int?");
+        }
 
         //        /// <summary>
         //        /// There are more way of changing value if you want to only increase x you can
